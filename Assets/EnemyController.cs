33186e269b5acc52dbcd307c8daf6397e00e31b8
cookieopjax <vigor01695@gmail.com>
2022-03-08 18:45:16 +0800

    add hp bar

--- Assets/EnemyController.cs
+++ Assets/EnemyController.cs
@@ -2,15 +2,33 @@ using UnityEngine;
 
 public class EnemyController : MonoBehaviour
 {  
-    void Update()
-    {
-        if (Input.GetKey(KeyCode.K))
-        {
-            Debug.Log(Vector3.Distance(transform.position, GameObject.FindGameObjectWithTag ("Player").transform.position));
-            
+    [SerializeField] int _hp = 0;
+    [SerializeField] int _hpMax = 0;
+    [SerializeField] GameObject hpBar;
+
+    void Start() {
+       _hpMax = 100;
+       _hp = _hpMax;
+    }
+    void Update() {
+        if (Input.GetKeyDown(KeyCode.K)) {
             //敵人與玩家的距離判定
-            if (Vector3.Distance(transform.position, GameObject.FindGameObjectWithTag ("Player").transform.position) <= 3)
-                gameObject.SetActive(false);
+            if (Vector3.Distance(transform.position, GameObject.FindGameObjectWithTag ("Player").transform.position) <= 3){
+                Debug.Log("有打中 !");
+                _hp -= 20;
+
+                if(_hp <= 0){ //敵人死亡
+                    hpBar.transform.localScale = new Vector3(0, hpBar.transform.localScale.y, hpBar.transform.localScale.z);
+                    Destroy(this.gameObject);
+                    return;
+                }
+
+                //敵人未死亡
+                float _percent =((float)_hp / (float)_hpMax);
+                hpBar.transform.localScale = new Vector3(_percent, hpBar.transform.localScale.y, hpBar.transform.localScale.z);
+                
+            }
+                
         }
     }
 }
